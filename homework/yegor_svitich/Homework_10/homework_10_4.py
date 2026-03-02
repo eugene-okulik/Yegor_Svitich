10768b8670196c5c69c5318abdda656b423ad929
Yegor Svitich <svitich.e@promail366.com>
2026-03-02 15:39:15 +0300

    Added 4 py-files for homework 10

---
+++ homework/yegor_svitich/Homework_10/homework_10_4.py
@@ -0,0 +1,14 @@
+PRICE_LIST = '''тетрадь 50р
+книга 200р
+ручка 100р
+карандаш 70р
+альбом 120р
+пенал 300р
+рюкзак 500р'''
+
+price_dict = {
+    line.split()[0]: int(line.split()[1].replace('р', ''))
+    for line in PRICE_LIST.split('\n')
+}
+
+print(price_dict)
