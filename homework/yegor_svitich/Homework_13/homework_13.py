216e6a42798db4b6c450caef59f2491e63de0355
Yegor Svitich <svitich.e@promail366.com>
2026-03-16 21:28:38 +0300

    Added blank lines

--- homework/yegor_svitich/Homework_13/homework_13.py
+++ homework/yegor_svitich/Homework_13/homework_13.py
@@ -11,12 +11,13 @@ with open(eugene_file_path, encoding='utf-8') as eugene_file:
     data = eugene_file.read()
     print(data)
 
+
 def process_dates(lines):
     for line in lines:
         line = line.strip()
         try:
-            number, rest = line.split('. ', 1) # отделяем номер задания до первой точки
-            date_str, text = rest.split(' - ', 1) # по дефису отделяем дату от текста
+            number, rest = line.split('. ', 1)  # отделяем номер задания до первой точки
+            date_str, text = rest.split(' - ', 1)  # по дефису отделяем дату от текста
             date = datetime.strptime(date_str.strip(), "%Y-%m-%d %H:%M:%S.%f")
             print(f'\nЗадание №{number}:')
             if number == '1':
@@ -31,6 +32,7 @@ def process_dates(lines):
                 print(f'Эта дата была {third_task.days} дней назад')
         except Exception as error:
             print(f"Ошибка при обработке строки '{line}': {error}")
+
 
 try:
     with open(eugene_file_path, 'r', encoding='utf-8') as data_file:
